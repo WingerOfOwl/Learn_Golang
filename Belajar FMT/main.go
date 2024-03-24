57dba18ae5e165615ed5d8739772d7546b849aef
WingerOfOwl <ahmadmuharamjiddanwahono@gmail.com>
2024-03-24 23:12:16 +0700

    add HelloWorld and Number

--- Belajar FMT/main.go
+++ Belajar FMT/main.go
@@ -1,8 +1,30 @@
 package main
 
-import cobafolder "cobaMod/CobaFolder"
+import "fmt"
 
 func main() {
-	cobafolder.Mantap()
-	cobafolder.CobaFile2()
+	Number()
+	HelloWorld()
+}
+
+func Number() {
+	satu := 1
+	lima := 5
+	tigakomalima := 3.5
+	fmt.Println("Angka = ", satu)
+	fmt.Printf("Angka = %d \n", lima)
+	fmt.Println("Angka = ", tigakomalima)
+
+}
+
+func HelloWorld() {
+	tatang := "komplit"
+	marsha := "Mie ayam"
+	Jiddan := 2
+	Della := Jiddan * 4
+	fmt.Println("mamang " + tatang)
+	fmt.Println(Della)
+	fmt.Printf("hallo %s %s \n", marsha, tatang) // ini di inject ke string "%s buat type string"
+	fmt.Printf("anak ku ada %d", Jiddan)         // ini di inject ke string "%d buat type int"
+
 }
